d37ea875e36ab8f60dea6e980ff1d569a12958a0
Mathias Buus <mathiasbuus@gmail.com>
2019-02-04 12:06:46 +0100

    bump node versions

--- test.js
+++ test.js
@@ -2,22 +2,22 @@ var tape = require('tape')
 var distance = require('./')
 
 tape('distance', function (t) {
-  t.same(distance(new Buffer([1, 0]), new Buffer([0, 1])), new Buffer([1, 1]))
-  t.same(distance(new Buffer([1, 1]), new Buffer([0, 1])), new Buffer([1, 0]))
-  t.same(distance(new Buffer([1, 1]), new Buffer([1, 1])), new Buffer([0, 0]))
+  t.same(distance(Buffer.from([1, 0]), Buffer.from([0, 1])), Buffer.from([1, 1]))
+  t.same(distance(Buffer.from([1, 1]), Buffer.from([0, 1])), Buffer.from([1, 0]))
+  t.same(distance(Buffer.from([1, 1]), Buffer.from([1, 1])), Buffer.from([0, 0]))
   t.end()
 })
 
 tape('compare', function (t) {
-  t.same(distance.compare(new Buffer([0, 0]), new Buffer([0, 1])), -1)
-  t.same(distance.compare(new Buffer([0, 1]), new Buffer([0, 1])), 0)
-  t.same(distance.compare(new Buffer([1, 1]), new Buffer([0, 1])), 1)
+  t.same(distance.compare(Buffer.from([0, 0]), Buffer.from([0, 1])), -1)
+  t.same(distance.compare(Buffer.from([0, 1]), Buffer.from([0, 1])), 0)
+  t.same(distance.compare(Buffer.from([1, 1]), Buffer.from([0, 1])), 1)
   t.end()
 })
 
 tape('shorthands', function (t) {
-  t.ok(distance.lt(new Buffer([0, 0]), new Buffer([0, 1])))
-  t.ok(distance.eq(new Buffer([0, 1]), new Buffer([0, 1])))
-  t.ok(distance.gt(new Buffer([1, 1]), new Buffer([0, 1])))
+  t.ok(distance.lt(Buffer.from([0, 0]), Buffer.from([0, 1])))
+  t.ok(distance.eq(Buffer.from([0, 1]), Buffer.from([0, 1])))
+  t.ok(distance.gt(Buffer.from([1, 1]), Buffer.from([0, 1])))
   t.end()
 })
